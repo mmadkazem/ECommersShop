3541573ffaf0c691602cd48f4d7bfbd5509729ae
mohammadKazem <mmadkzm@gmail.com>
2023-12-12 17:58:47 +0330

    modified User In roles Entity

--- Entity/UserInRole.cs
+++ Entity/UserInRole.cs
@@ -4,7 +4,7 @@ namespace ECommersShop.Entity
 {
     public class UserInRole : BaseEntity
     {
-        public virtual User? User { get; set; }
+        public required virtual User User { get; set; }
         public int UserId { get; set; }
         public Role? Role { get; set; }
     }
