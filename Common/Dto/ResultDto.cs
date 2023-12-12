5f0154abd0dee62944c363909ecd8eb6bd523e20
mohammadKazem <mmadkzm@gmail.com>
2023-12-12 11:49:07 +0330

    added ResultDto for Return

---
+++ Common/Dto/ResultDto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommersShop.Common.Dto
+{
+    public class ResultDto
+    {
+        public bool IsSucssecc { get ; set; }
+        public string? Message { get ; set; }
+    }
+    public class ResultDto<T>
+    {
+        public T? Data { get; set; }
+        public bool IsSucssecc { get ; set; }
+        public string? Message { get ; set; }
+    }
+    public class ResultsDto<T>
+    {
+        public List<T>? Data { get; set; }
+        public bool IsSucssecc { get ; set; }
+        public string? Message { get ; set; }
+    }
+
+}
