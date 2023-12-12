afd3fe93bfbb8b88113165012cf12e3a34be34cd
mohammadKazem <mmadkzm@gmail.com>
2023-12-12 11:47:23 +0330

    added BaseEntity

---
+++ Common/Model/BaseEntity.cs
@@ -0,0 +1,15 @@
+namespace ECommersShop.Common.Model
+{
+    public abstract class BaseEntity<TKey>  
+    {
+        public TKey? Id { get; set; }
+        public DateTime InsertTime { get; set; } = DateTime.Now;
+        public DateTime? UpdateTime { get; set; }
+        public bool IsRemoved { get; set; } = false;
+        public DateTime? RemoveTime { get; set; }
+    }
+    public abstract class BaseEntity : BaseEntity<int>
+    {
+    }
+
+}
